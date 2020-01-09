81217d4a9b95539ad857dbaca50be4abc62938fc
yamamotooo <47420311+yamamotooo@users.noreply.github.com>
2020-01-09 12:08:14 +0900

    Add files via upload

--- generateTables.go
+++ generateTables.go
@@ -6,38 +6,48 @@ import (
 	"os/exec"
 	"strings"
 
-	"github.com/tealeg/xlsx"
+	"os"
+	"path/filepath"
+
+	"github.com/tealeg/xlsx" //go get github.com/tealeg/xlsx
+	"runtime"
+
+	"./clipboard"
 )
 
-var snippetXML = `<fmxmlsnippet type="FMObjectList">%s</fmxmlsnippet>`
-var tableXML = `<BaseTable name="%s">%s</BaseTable>`
-var fieldXML = `<Field id="%s" dataType="%s" fieldType="%s" name="%s">
-		<Comment>%s
-		</Comment>
-		<AutoEnter allowEditing="%s" constant="%s" furigana="%s" lookup="%s" calculation="%s">
-		<ConstantData>%s
-		</ConstantData>
-		</AutoEnter>
-		<Validation message="%s" maxLength="%s" valuelist="%s" calculation="%s" alwaysValidateCalculation="%s" type="%s">
-		<NotEmpty value="%s">
-		</NotEmpty>
-		<Unique value="%s">
-		</Unique>
-		<Existing value="%s">
-		</Existing>
-		<StrictValidation value="%s">
-		</StrictValidation>
-		</Validation>
-		<Storage autoIndex="%s" index="%s" indexLanguage="%s" global="%s" maxRepetition="%s">
-		</Storage>
-	</Field>`
+var (
+	snippetXML	= `<fmxmlsnippet type="FMObjectList">%s</fmxmlsnippet>`
+	tableXML	= `<BaseTable name="%s">%s</BaseTable>`
+	fieldXML	= `<Field id="%s" dataType="%s" fieldType="%s" name="%s">
+					<Comment>%s
+					</Comment>
+					<AutoEnter allowEditing="%s" constant="%s" furigana="%s" lookup="%s" calculation="%s">
+					<ConstantData>%s
+					</ConstantData>
+					</AutoEnter>
+					<Validation message="%s" maxLength="%s" valuelist="%s" calculation="%s" alwaysValidateCalculation="%s" type="%s">
+					<NotEmpty value="%s">
+					</NotEmpty>
+					<Unique value="%s">
+					</Unique>
+					<Existing value="%s">
+					</Existing>
+					<StrictValidation value="%s">
+					</StrictValidation>
+					</Validation>
+					<Storage autoIndex="%s" index="%s" indexLanguage="%s" global="%s" maxRepetition="%s">
+					</Storage>
+				</Field>`
+)
 
 func main() {
 
-	xlsxPath := "your file path here or use below"
-	//xlsxPath, err := os.Executable()
-	//xlsxPath = filepath.Dir(xlsxPath) + "/Book1.xlsx"
-
+	//xlsxPath := "your file path here or use below"
+	xlsxPath, err := os.Executable()
+	//xlsxPath, err := os.Getwd()
+	xlsxPath = filepath.Dir(xlsxPath) + "/Book1.xlsx"
+	//fmt.Println(xlsxPath)
+	
 	xlsxFile, err := xlsx.OpenFile(xlsxPath)
 	if err != nil {
 		panic("Excel file not found...")
@@ -60,13 +70,30 @@ func main() {
 	snippetXML = fmt.Sprintf(snippetXML, tables)
 	snippetXML = strings.NewReplacer("\t", "", "\r\n", "", "\r", "", "\n", "").Replace(string(snippetXML))
 
-	hexData := hex.EncodeToString([]byte(snippetXML))
-	hexData = "XMTB" + hexData
-	hexData = "«data " + hexData + "»"
-
-	err = exec.Command("osascript", "-e", fmt.Sprintf("set the clipboard to %s", hexData)).Run()
-	if err != nil {
-		panic(err)
+	switch runtime.GOOS {
+	case "darwin":
+		hexData := hex.EncodeToString([]byte(snippetXML))
+		hexData = "XMTB" + hexData
+		hexData = "«data " + hexData + "»"
+		err = exec.Command("osascript", "-e", fmt.Sprintf("set the clipboard to %s", hexData)).Run()
+		if err != nil {
+			panic(err)
+		}
+		//fallthrough 次節へ処理を流す場合
+	case "windows":
+		clipboard.WriteAll(snippetXML)
+		//text, _ := clipboard.ReadAll()
+		//fmt.Println(text)
+		//fallthrough 次節へ処理を流す場合
+	default:
+		fmt.Println("ohter")
 	}
 
 }
+
+//GOOS=darwin GOARCH=amd64 go build generateTables.go
+//GOOS=windows GOARCH=amd64 go build generateTables.go
+
+
+
+
